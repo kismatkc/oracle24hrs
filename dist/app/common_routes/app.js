69af346208b7ee75f366eac03b6c2aa1caaf3deb
kismatkc <kismatkc28@gmail.com>
2025-05-01 17:22:49 -0400

    Auto

---
+++ dist/app/common_routes/app.js
@@ -0,0 +1,27 @@
+import express, { json } from "express";
+import CORS from "cors";
+import modifyStops from "./controllers/modify-stops.js";
+import streaks from "./controllers/streaks.js";
+import getTtcTimes from "./controllers/ttc-times.js";
+import weatherReport from "./controllers/weather-report.js";
+import wordBreakdown from "./controllers/word-breakdown.js";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+app.use(CORS());
+app.use(json());
+const PORT = process.env.PORT_common; // Corrected environment variable
+if (!PORT) {
+    throw new Error("Please provide a valid port");
+}
+app.use("/", modifyStops);
+app.use("/", streaks);
+app.use("/", getTtcTimes);
+app.use("/", weatherReport);
+app.use("/", wordBreakdown);
+app.use("/", async (req, res) => {
+    res.send("Common routes");
+});
+app.listen(PORT, () => {
+    console.log("Listening on port", PORT);
+});
